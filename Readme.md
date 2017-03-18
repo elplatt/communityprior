a2af3c9721433eebb385117368cbdb1056e6bfb8
Edward L Platt <ed@elplatt.com>
2017-03-18 19:01:22 -0400

    Initial import.

---
+++ Readme.md
@@ -0,0 +1,20 @@
+## Estimate SSN-LDA[1] Dirichlet parameters from node communities.
+The estimation uses the MLE method described by `[1]` and implemented by (https://github.com/ericsuh/dirichlet).
+
+The input csv should be comma separated with no spaces and a header row.
+All ids should be numeric and start at 0.
+The strength of a node's community membership (`member_prob`) should be a real number
+between 0 and 1.
+The format is:
+
+    node_id,community_id,member_prob
+
+If the csv containing the community data is `examples/example.csv`, then the usage is:
+
+    python communityprior.py examples/example.csv alpha.csv beta.csv
+    
+Two files containing the priors, `alpha.csv` and `beta.csv`, will be written to the current directory.
+
+### References
+1. Zhang, H., Qiu, B., Giles, C. L., Foley, H. C., & Yen, J. (2007, May). An LDA-based community structure discovery approach for large-scale social networks. In _Intelligence and Security Informatics_, 2007 IEEE (pp. 200-207). IEEE.  
+2. Minka, T. (2000). Estimating a Dirichlet distribution.
---
+++ communityprior.py
@@ -0,0 +1,50 @@
+import sys
+
+import dirichlet
+import numpy
+import pandas
+
+def data_to_matrix(com_data):
+    # Count number of nodes and communities
+    # Assume the first of each is "0"
+    num_nodes = com_data['node_id'].max() + 1
+    num_coms = com_data['community_id'].max() + 1
+    
+    # Convert to matrix
+    node_com = numpy.zeros((num_nodes,num_coms))
+    for i, row in com_data.iterrows():
+        node_com[int(row["node_id"]),int(row["community_id"])] = row["member_prob"]
+    
+    return node_com
+
+def normalize_rows(m):
+    return m / m.sum(axis=1)[:,numpy.newaxis]
+
+def load_as_matrix(filename):
+    # node_id, community_id, member_prob
+    com_data = pandas.DataFrame.from_csv(filename, index_col=None)
+    return data_to_matrix(com_data)
+
+if __name__ == '__main__':
+    # Load
+    node_com = load_as_matrix(sys.argv[1])
+    
+    # Convert rows/cols into probability distributions
+    p_com = normalize_rows(node_com)
+    p_node = normalize_rows(node_com.transpose())
+    
+    # Estimate dirichlet parameter for distribution over communities (topics)
+    alpha = dirichlet.mle(p_com)
+    
+    # Estimate dirichlet parameter for distribution over nodes (terms)
+    beta = dirichlet.mle(p_node)
+
+    # Write output
+    with open(sys.argv[2], "wb") as f:
+        f.write("alpha_k\n")
+        for alpha_k in alpha:
+            f.write("%f\n" % alpha_k)
+    with open(sys.argv[3], "wb") as f:
+        f.write("beta_v\n")
+        for beta_v in beta:
+            f.write("%f\n" % beta_v)
