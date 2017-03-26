6cfdb882a22b525b72baabe2b01ba0906b842009
Edward L Platt <ed@elplatt.com>
2017-03-26 10:41:53 -0400

    Update counting script.

---
+++ count_communities.py
@@ -0,0 +1,16 @@
+import sys
+
+com_file = sys.argv[1]
+
+communities = set()
+nodes = set()
+
+with open(com_file) as f_com:
+    f_com.next()
+    for row in f_com:
+        node_id, community_id, member_prob = row.rstrip().split(",")
+        communities.add(community_id)
+        nodes.add(node_id)
+
+print "%d communities, %d - %d" % (len(communities), min(list(communities)), max(list(communities)))
+print "%d nodes, %d - %d" % (len(nodes), min(list(nodes)), max(list(nodes)))
