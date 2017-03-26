fdb57a2617402ed570e61c650c4c3f6f6ef3244a
Edward L Platt <ed@elplatt.com>
2017-03-26 10:42:31 -0400

    Cast to int.

--- count_communities.py
+++ count_communities.py
@@ -9,8 +9,8 @@ with open(com_file) as f_com:
     f_com.next()
     for row in f_com:
         node_id, community_id, member_prob = row.rstrip().split(",")
-        communities.add(community_id)
-        nodes.add(node_id)
+        communities.add(int(community_id))
+        nodes.add(int(node_id))
 
 print "%d communities, %d - %d" % (len(communities), min(list(communities)), max(list(communities)))
 print "%d nodes, %d - %d" % (len(nodes), min(list(nodes)), max(list(nodes)))
