c2cac97cf6166f5e695e359398c570b8d8252ea7
Edward L Platt <ed@elplatt.com>
2017-04-04 14:13:25 -0400

    No index column.

--- find_nmi.py
+++ find_nmi.py
@@ -5,8 +5,8 @@ import nmi
 test_file = sys.argv[1]
 truth_file = sys.argv[2]
 
-df_test = pd.DataFrame.from_csv(test_file)
-df_truth = pd.DataFrame.from_csv(truth_file)
+df_test = pd.DataFrame.from_csv(test_file, index_col=None)
+df_truth = pd.DataFrame.from_csv(truth_file, index_col=None)
 
 res = nmi.weighted_overlapping(df_test, df_truth)
 
