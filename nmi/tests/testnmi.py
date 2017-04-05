3bb7019c61d15accc6698f2538eb572592129437
Edward L Platt <ed@elplatt.com>
2017-04-04 23:52:57 -0400

    Add tests.

--- nmi/tests/testnmi.py
+++ nmi/tests/testnmi.py
@@ -1,7 +1,10 @@
 import unittest
 import numpy as np
+import numpy.testing as nptest
 import pandas as pd
 import nmi
+
+# Hand calculations in ELP-UM-002 p39
 
 df_a = pd.DataFrame.from_dict({
     "node_id": [0, 0, 1],
@@ -25,12 +28,33 @@ true_weights_b = np.array([
     [0.25, 0.75]
 ])
 
+true_a_b = np.array([
+    [0.375, 0.25],
+    [0.5, 0.625]
+])
+
+true_a_notb = np.array([
+    [0.125, 0.25],
+    [0.375, 0.25]
+])
+
+true_nota_b = np.array([
+    [0.125, 0.375],
+    [0, 0]
+])
+
 class TestAll(unittest.TestCase):
     
     def test_weights(self):
         weights_a, weights_b = nmi.get_weights(df_a, df_b, normalize=False)
-        self.assertEqual(weights_a, true_weights_a)
-        self.assertEqual(weights_b, true_weights_b)
+        nptest.assert_array_equal(weights_a, true_weights_a)
+        nptest.assert_array_equal(weights_b, true_weights_b)
+    
+    def test_joint(self):
+        a_b, a_notb, nota_b = nmi.get_joint_dist(true_weights_a, true_weights_b)
+        nptest.assert_array_almost_equal(a_b, true_a_b)
+        nptest.assert_array_almost_equal(a_notb, true_a_notb)
+        nptest.assert_array_almost_equal(nota_b, true_nota_b)
         
 if __name__ == '__main__':
     unittest.main()
