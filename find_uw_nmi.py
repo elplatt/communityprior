d7efb6deab24b4b6f080662f7d93796efba6a4cc
Platt <elplatt@m-c02t32cyg8wl.local>
2017-04-07 16:39:59 -0400

    Add command for finding unweighted nmi.

---
+++ find_uw_nmi.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+import nmi
+
+test_file = sys.argv[1]
+truth_file = sys.argv[2]
+
+df_test = pd.DataFrame.from_csv(test_file, index_col=None)
+df_truth = pd.DataFrame.from_csv(truth_file, index_col=None)
+
+res = nmi.unweighted_overlapping(df_test, df_truth)
+
+print "NMI: ", res
