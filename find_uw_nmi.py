90824397c91082dddbce4f0ece4cb651d194ea01
Edward L Platt <ed@elplatt.com>
2017-04-07 20:31:47 -0400

    Add threshhold to unweighted nmi script.

--- find_uw_nmi.py
+++ find_uw_nmi.py
@@ -4,10 +4,13 @@ import nmi
 
 test_file = sys.argv[1]
 truth_file = sys.argv[2]
-
+try:
+    threshold = float(sys.argv[3])
+except IndexError:
+    threshold = 1.0
 df_test = pd.DataFrame.from_csv(test_file, index_col=None)
 df_truth = pd.DataFrame.from_csv(truth_file, index_col=None)
 
-res = nmi.unweighted_overlapping(df_test, df_truth)
+res = nmi.unweighted_overlapping(df_test, df_truth, threshold)
 
 print "NMI: ", res
