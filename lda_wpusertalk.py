5d0fb8c2d6029a43f860d26ea38819a66ca2349c
Edward L Platt <ed@elplatt.com>
2017-04-11 10:32:31 -0400

    Add timestamp to wpusertalk.

--- lda_wpusertalk.py
+++ lda_wpusertalk.py
@@ -1,11 +1,12 @@
 import logging
 import sys
+import time
 import gensim
 import gensim.models
 import corpus.WPCorpus
 
 dict_file = "data/networks/wpuser-dict.csv"
-out_file = "output/communities/wpusertalk-simplelda-%d.csv"
+out_file = "output/communities/wpusertalk-simplelda-%d-%s.csv"
 # Ground truth: 12093
 num_topics = int(sys.argv[1])
 num_words = 6470
@@ -13,6 +14,8 @@ num_words = 6470
 logging.basicConfig(filename='logs/gensim-wpusertalk-simple-%d.log' % num_topics, format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 wpc = corpus.WPCorpus.WPCorpus()
 wpm = gensim.models.LdaModel(wpc, num_topics=num_topics)
+
+timestamp = time.strftime("%m%dT%H%M")
 
 # Load dictionary
 index_to_id = {}
@@ -22,7 +25,7 @@ with open(dict_file, "rb") as f_dict:
         node_index, node_id = row.rstrip().split(",")
         index_to_id[int(node_index)] = int(node_id)
 
-with open(out_file % num_topics, "wb") as f_out:
+with open(out_file % (num_topics, timestamp), "wb") as f_out:
     f_out.write("node_id,community_id,member_prob\n")
     for topic in range(num_topics):
         try:
